4f8e4a60a60a1ad0417fe68fd201c2b691606e90
Ahmed Magdy <ahmed.magdy@woven-planet.global>
2023-12-13 11:56:04 +0900

    Adjust instructions

--- 02_more_strings_and_control_flow/01_challenge/temp_convert.py
+++ 02_more_strings_and_control_flow/01_challenge/temp_convert.py
@@ -1,7 +1,6 @@
 def convert():
     # your code here
-    # edit the temp variable to take the temperature degree as input from the user.
-    # Don't forget to convert it to an integer
+    # Edit the temp variable to take the temperature degree as input from the user. Don't forget to convert it to an integer
     temp = 0
 
     return temp
