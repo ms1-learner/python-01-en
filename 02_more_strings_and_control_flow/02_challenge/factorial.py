48e79c9f1614bfb2a6c14a2fc3666455636998bc
woven-baker <lewis.baker@woven-planet.global>
2024-01-24 21:52:13 +0900

    fixed instructions

--- 02_more_strings_and_control_flow/02_challenge/factorial.py
+++ 02_more_strings_and_control_flow/02_challenge/factorial.py
@@ -1,6 +1,6 @@
 def compute_factorial():
     # Your code here
-    # Edit the number variable to take the temperature degree as input from the user. Don't forget to convert it to an integer
+    # Edit the number variable to take a positive integer as input from the user. Don't forget to convert it to an integer.
     number = 0
 
     # Edit the result variable to store the final result of the computation
